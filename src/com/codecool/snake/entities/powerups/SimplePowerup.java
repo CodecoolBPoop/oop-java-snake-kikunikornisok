1fafa6347c2910c6f68ef61c1cb35fe8bf846d2a
Grob Oliver <groboliver1117@gmail.com>
2018-07-10 13:17:52 +0200

    enemy, powerup will not spawn in wall

--- src/com/codecool/snake/entities/powerups/SimplePowerup.java
+++ src/com/codecool/snake/entities/powerups/SimplePowerup.java
@@ -17,8 +17,8 @@ public class SimplePowerup extends GameEntity implements Interactable {
         pane.getChildren().add(this);
 
         Random rnd = new Random();
-        setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
-        setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
+        setX(rnd.nextDouble() * (Globals.WINDOW_WIDTH - 50));
+        setY(rnd.nextDouble() * (Globals.WINDOW_HEIGHT - 50));
     }
 
     @Override
