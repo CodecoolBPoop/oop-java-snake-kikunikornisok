048343752b7ca38ccce0e018e97901eb8b238c3c
Grob Oliver <groboliver1117@gmail.com>
2018-07-11 11:21:20 +0200

    Random spawn simplepowerup

--- src/com/codecool/snake/Game.java
+++ src/com/codecool/snake/Game.java
@@ -6,6 +6,8 @@ import com.codecool.snake.entities.snakes.SnakeHead;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 
+import java.util.Random;
+
 public class Game extends Pane {
 
     public Game() {
@@ -25,6 +27,7 @@ public class Game extends Pane {
     public void start() {
         Scene scene = getScene();
         scene.setOnKeyPressed(event -> {
+            spawnEnemies();
             switch (event.getCode()) {
                 case LEFT:  Globals.leftKeyDown  = true; break;
                 case RIGHT: Globals.rightKeyDown  = true; break;
@@ -32,12 +35,27 @@ public class Game extends Pane {
         });
 
         scene.setOnKeyReleased(event -> {
+            spawnEnemies();
             switch (event.getCode()) {
                 case LEFT:  Globals.leftKeyDown  = false; break;
                 case RIGHT: Globals.rightKeyDown  = false; break;
             }
         });
+        spawnEnemies();
         Globals.gameLoop = new GameLoop();
         Globals.gameLoop.start();
+    }
+
+    public void spawnEnemies() {
+        Random random = new Random();
+        int randomNum = random.nextInt(30);
+
+        if (randomNum == 15) {
+            new SimplePowerup(this);
+//        } else if (randomNum == 25) {
+//            new DecreasingSpeedPowerUp(this);
+        } else if (randomNum == 5) {
+            new IncreasingSpeedEnemy(this);
+        }
     }
 }
